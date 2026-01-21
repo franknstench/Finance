54bc371fa09e64d744b4c793540f776dadf39d6c
franknstench <96508836+franknstench@users.noreply.github.com>
2026-01-21 09:58:05 -0800

    Create StockAPI.swift file for stock data fetching

---
+++ Services/StockAPI.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/// This file will eventually fetch real stock data from an API provider.
+/// For now, it is a placeholder with clear comments for future steps.
+
+struct StockAPI {
+    // TODO: In the future, add functions here to fetch data using your API key.
+    // You will use networking (URLSession) to pull NYSE opening & closing data.
+    // When you get an API key (e.g., from Alpha Vantage), ask for further guidance!
+}
